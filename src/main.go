52bd5c380e7610d505c7936d77951a510bd7fe5f
Nicholas Hellmers <nihe1661@colorado.edu>
2023-12-26 10:37:41 -0300

    Changed: .gitignore docker-complose-dev.yaml files

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	app := fiber.New()
 
+	// &fiber.Map is a shortcut for map[string]interface{} (useful for JSON returns)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
 	})
